99cd8ead8275c90dda5808264a747a5b6ead283c
Gerardo vasquez <gvsvasquez90@gmail.com>
2024-05-30 16:44:39 -0600

    validar el token

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -14,6 +14,7 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        System.out.println("El filtro se esta llamando");
         filterChain.doFilter(request,response);
     }
 }
