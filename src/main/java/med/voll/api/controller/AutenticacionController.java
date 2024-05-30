fef14d8c1f864f0e16de4eaf5664d097a8326cc9
Gerardo vasquez <gvsvasquez90@gmail.com>
2024-05-30 14:48:37 -0600

    JWT

--- src/main/java/med/voll/api/controller/AutenticacionController.java
+++ src/main/java/med/voll/api/controller/AutenticacionController.java
@@ -3,6 +3,9 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 
 import med.voll.api.domain.usuarios.DatosAutenticacionUsuario;
+import med.voll.api.domain.usuarios.Usuario;
+import med.voll.api.infra.security.DatosJWTToken;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -21,11 +24,15 @@ public class AutenticacionController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity<?> autenticarUsuario(@RequestBody @Valid DatosAutenticacionUsuario datos) {
-        Authentication token = new UsernamePasswordAuthenticationToken(datos.login(), datos.clave());
-        authenticationManager.authenticate(token);
+        Authentication authToken = new UsernamePasswordAuthenticationToken(datos.login(), datos.clave());
+        var usuarioAutenticado = authenticationManager.authenticate(authToken);
+        var JWTtoken = tokenService.generarToken((Usuario) usuarioAutenticado.getPrincipal());
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(new DatosJWTToken(JWTtoken));
     }
 }
